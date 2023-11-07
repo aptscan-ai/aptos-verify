ca368e37f66ce7624ecd2c1bed4f175a0ff16899
Phong <phongbro1805@gmail.com>
2023-11-07 11:30:34 +0700

    add decompress bytecode

--- aptos_verify/rules/compare_bytecode.py
+++ aptos_verify/rules/compare_bytecode.py
@@ -1,16 +1,42 @@
 # This rule will check code that public onchain with current bytecode of one module_id
-from aptos_verify.utils import aptos_rest_client, rpc_account_get_bytecode, rpc_account_get_source_code
+from aptos_verify.utils import AptosUtils, AptosBytecodeUtils
 from aptos_verify.schemas import CmdArgs
 from aptos_verify.config import get_logger
-import aptos_verify.memory as memory
+import asyncio
 
 logger = get_logger(__name__)
 
 
-
 async def process_compare_bycode(args: CmdArgs, **krawgs):
-    account, func_name = args.module_id.split('::')
-    source_code = await rpc_account_get_source_code(account_address=account, func_name=func_name)
-    print(source_code.get('source'))
-    
-    # await get_bytecode_onchain(args.module_id)
+    """
+    This code will compare bytecode from onchain and source code thats deployed and published onchain
+    """
+    account, module_name = args.module_id.split('::')
+
+    async def get_source_code_onchain():
+        source_code = await AptosUtils.rpc_account_get_source_code(account_address=account, module_name=module_name)
+        bytecode = source_code.get('source')
+        package = source_code.get('package')
+        decompressed_source_code = AptosBytecodeUtils.decompress_bytecode(
+            bytecode)
+        manifest = AptosBytecodeUtils.decompress_bytecode(
+            package.get('manifest'))
+        package_name = package.get('name')
+        return {
+            'source_code': decompressed_source_code,
+            'manifest': manifest,
+            'package_name':  package_name
+        }
+
+    async def get_bytecode_onchain():
+        bytecode = await AptosUtils.rpc_account_get_bytecode(account_address=account, module_name=module_name)
+        return bytecode
+
+    source_code_info, bytecode = await asyncio.gather(
+        get_source_code_onchain(),
+        get_bytecode_onchain()
+    )
+
+    print(source_code_info, bytecode)
+
+    logger.info("Compare bytecode done.")
--- aptos_verify/utils.py
+++ aptos_verify/utils.py
@@ -6,80 +6,105 @@ from aptos_verify.config import get_logger
 import aptos_verify.exceptions as verify_exceptions
 import pydantic
 import typing
+import zlib
+from collections import namedtuple
 
 logger = get_logger(__name__)
 
 
-async def aptos_rest_client(**options) -> RestClient:
-    """
-    Init rest client instance that will be used to work with RPC API
-    Docs: https://pypi.org/project/aptos-sdk/
-    """
-    cf = get_config()
-    return RestClient(
-        base_url=cf.aptos_node_url,
-        **options
-    )
+class AptosUtils:
 
+    @staticmethod
+    async def aptos_rest_client(**options) -> RestClient:
+        """
+        Init rest client instance that will be used to work with RPC API
+        Docs: https://pypi.org/project/aptos-sdk/
+        """
+        cf = get_config()
+        return RestClient(
+            base_url=cf.aptos_node_url,
+            **options
+        )
 
-@pydantic.validate_call
-async def rpc_account_get_package(account_address: typing.Annotated[str, Field(min_length=5)], **option) -> list[dict]:
-    """
-    Get resources of an account by given account address
-    """
-    client = await aptos_rest_client()
-    logger.info(
-        f'Call Aptos RPC to get resoures of account: {account_address}')
-    key = f'local_cache_account_package_{account_address}'
-    rs = local_memory.get(key=key)
-    if local_memory.get(key=key):
-        return rs
-    resources = await client.account_resource(
-        account_address=account_address, resource_type='0x1::code::PackageRegistry')
-    if resources:
-        rs = resources.get('data', {}).get('packages')
+    @staticmethod
+    @pydantic.validate_call
+    async def rpc_account_get_package(account_address: typing.Annotated[str, Field(min_length=5)], **option) -> list[dict]:
+        """
+        Get resources of an account by given account address
+        """
+        client = await AptosUtils.aptos_rest_client()
+        logger.info(
+            f'Call Aptos RPC to get resoures of account: {account_address}')
+        key = f'local_cache_account_package_{account_address}'
+        rs = local_memory.get(key=key)
+        if local_memory.get(key=key):
+            return rs
+        resources = await client.account_resource(
+            account_address=account_address, resource_type='0x1::code::PackageRegistry')
+        if resources:
+            rs = resources.get('data', {}).get('packages')
+            local_memory.set(key=key, value=rs)
+            return rs
+        raise verify_exceptions.PackagesNotFoundException()
+
+    @staticmethod
+    @pydantic.validate_call
+    async def rpc_account_get_source_code(account_address: typing.Annotated[str, Field(min_length=5)],
+                                          module_name: typing.Annotated[str, Field(min_length=1)]) -> str:
+        """
+        Get source code of a module
+        """
+        packages = await AptosUtils.rpc_account_get_package(account_address=account_address)
+        for package in packages:
+            for module in package.get('modules', []):
+                if module.get('name') == module_name:
+                    return {
+                        'source': module.get('source'),
+                        'source_map': module.get('source_map'),
+                        'module_name': module.get('name'),
+                        'package': package
+                    }
+        raise verify_exceptions.ModuleNotFoundException()
+
+    @staticmethod
+    @pydantic.validate_call
+    async def rpc_account_get_bytecode(account_address: typing.Annotated[str, Field(min_length=5)],
+                                       module_name: typing.Annotated[str, Field(min_length=1)]) -> str:
+        logger.info(
+            f'Start get bytecode of module: {account_address}::{module_name}')
+        key = f'local_cache_account_module_bytecode_{account_address}_{module_name}'
+        rs = local_memory.get(key=key)
+        if local_memory.get(key=key):
+            return rs
+        sdk_client = await AptosUtils.aptos_rest_client()
+        client = sdk_client.client
+        request = f"{sdk_client.base_url}/accounts/{account_address}/module/{module_name}"
+        response = await client.get(request)
+        if response.status_code >= 400:
+            raise verify_exceptions.ApiError(f"{response.text} - {account_address}",
+                                             response.status_code)
+
+        rs = response.json()
         local_memory.set(key=key, value=rs)
         return rs
-    raise verify_exceptions.PackagesNotFoundException()
 
 
-@pydantic.validate_call
-async def rpc_account_get_source_code(account_address: typing.Annotated[str, Field(min_length=5)],
-                                      func_name: typing.Annotated[str, Field(min_length=1)]) -> str:
-    """
-    Get source code of a module
-    """
-    packages = await rpc_account_get_package(account_address=account_address)
-    for package in packages:
-        moduel_name = package.get('name', '')
-        for module in package.get('modules', []):
-            if module.get('name') == func_name:
-                return {
-                    'source': module.get('source'),
-                    'source_map': module.get('source_map'),
-                    'func_name': module.get('name'),
-                    'moduel_name': moduel_name
-                }
-    raise verify_exceptions.ModuleNotFoundException()
+class AptosBytecodeUtils:
 
+    @staticmethod
+    @pydantic.validate_call
+    def decompress_bytecode(hex_string: typing.Annotated[str, Field(min_length=5)]) -> str:
+        unit8_hex_bytes = bytearray(
+            bytes.fromhex(hex_string.replace('0x', '')))
+        decompressed_data = zlib.decompress(unit8_hex_bytes, 15+32)
+        decompressed_source_code = str(decompressed_data.decode('utf-8'))
+        return decompressed_source_code
 
-@pydantic.validate_call
-async def rpc_account_get_bytecode(account_address: typing.Annotated[str, Field(min_length=5)],
-                                   func_name: typing.Annotated[str, Field(min_length=1)]) -> str:
-    logger.info(
-        f'Start get bytecode of module: {account_address}::{func_name}')
-    key = f'local_cache_account_module_bytecode_{account_address}_{func_name}'
-    rs = local_memory.get(key=key)
-    if local_memory.get(key=key):
-        return rs
-    sdk_client = await aptos_rest_client()
-    client = sdk_client.client
-    request = f"{sdk_client.base_url}/accounts/{account_address}/module/{func_name}"
-    response = await client.get(request)
-    if response.status_code >= 400:
-        raise verify_exceptions.ApiError(f"{response.text} - {account_address}",
-                                         response.status_code)
-
-    rs = response.json()
-    local_memory.set(key=key, value=rs)
-    return rs
+    @staticmethod
+    @pydantic.validate_call
+    def extract_bytecode_from_build(path: typing.Annotated[str, Field(min_length=1)]) -> str:
+        """
+        This method will extract bytecode from a build project move
+        """
+        bytecode = ''
+        return bytecode
